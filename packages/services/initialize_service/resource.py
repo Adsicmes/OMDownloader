f9fac0d179f0256fed11036a72dd7d31e804296e
frz <1483492332@qq.com>
2023-06-30 17:07:22 +0800

    Fix auto clean resource import error in jetbrains ideas

--- packages/services/initialize_service/resource.py
+++ packages/services/initialize_service/resource.py
@@ -3,4 +3,5 @@ from loguru import logger
 
 def initResources():
     logger.info("Initializing resources...")
+    from packages import resources  # noqa # pylint: disable=unused-import
     logger.info("Successfully initialized resources.")
