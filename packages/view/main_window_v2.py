b7e1a532d8102aed5280606da94b5da577735024
frz <1483492332@qq.com>
2023-07-06 01:05:43 +0800

    Splash Screen

--- packages/view/main_window_v2.py
+++ packages/view/main_window_v2.py
@@ -1,13 +1,17 @@
 import i18n
+from PySide6.QtCore import QSize
 from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import QApplication
-from qfluentwidgets import FluentWindow
+from qfluentwidgets import FluentWindow  # , SplashScreen
 from qfluentwidgets.common.icon import FluentIcon as FIF
 from qfluentwidgets.common.style_sheet import setTheme, Theme
 from qfluentwidgets.components.navigation.navigation_interface import NavigationItemPosition
+from qframelesswindow import TitleBar
 
 from packages.config import config
-from .components import AvatarWidget
+from .components import (AvatarWidget,
+                         SplashScreenWithFadeOut as SplashScreen,
+                         FluentTitleBarWithVersionNumber as TitleBar)
 from .pages import *
 
 
@@ -15,13 +19,13 @@ class MainWindow(FluentWindow):
 
     def __init__(self):
         super().__init__()
+        self.initWindow()
 
         # create sub interface
         self.homeInterface = HomeInterface(self)
         self.mapDownloadInterface = MapDownloadInterface(self)
         self.initNavigation()
-
-        self.initWindow()
+        self.splashScreen.finish()
 
     def initNavigation(self):
         # add navigation items
@@ -53,13 +57,21 @@ class MainWindow(FluentWindow):
             setTheme(Theme.AUTO)
 
     def initWindow(self):
+        self.setTitleBar(TitleBar(self))
         self.resize(1300, int(1300 * 9 / 16))
         self.setMinimumWidth(1300)
         self.setMinimumHeight(int(1300 * 9 / 16))
         self.setWindowIcon(QIcon(':res/raw/osu_icon.png'))
         self.setWindowTitle(i18n.t("app.mainWindow.appTitle"))
+
         self.setApplicationTheme()
+
+        self.splashScreen = SplashScreen(self.windowIcon(), self)
+        self.splashScreen.setIconSize(QSize(106, 106))
+        self.splashScreen.raise_()
 
         desktop = QApplication.screens()[0].availableGeometry()
         w, h = desktop.width(), desktop.height()
         self.move(w // 2 - self.width() // 2, h // 2 - self.height() // 2)
+        self.show()
+        QApplication.processEvents()
