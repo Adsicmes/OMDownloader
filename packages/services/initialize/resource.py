a2cf89376496ec5dcc2884efd605cca917818c11
frz <1483492332@qq.com>
2023-07-25 10:58:17 +0800

    fix:  Resource import error

--- packages/services/initialize/resource.py
+++ packages/services/initialize/resource.py
@@ -3,5 +3,5 @@ from loguru import logger
 
 def initResources():
     logger.info("Initializing resources...")
-    from packages import resources  # noqa # pylint: disable=unused-import
+    from resources import resources  # noqa # pylint: disable=unused-import
     logger.info("Successfully initialized resources.")
